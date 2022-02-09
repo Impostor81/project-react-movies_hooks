c86579963e27721aa208f64be57e3c02e6dc7611
Aleksandr Maltsev <0po4ta@gmail.com>
2022-02-09 18:46:31 +0200

    fixed links repo

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   <div className="nav-wrapper">
     <a href="#" className="logo">React Movies</a>
     <ul id="nav-mobile" className="right">
-      <li><a href="https://github.com/Impostor81/project-react-movies">Repo</a></li>
+      <li><a href="https://github.com/Impostor81/project-react-movies_hooks">Repo</a></li>
     </ul>
   </div>
 </nav>;
